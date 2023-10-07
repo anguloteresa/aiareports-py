140eea3951e6965c3e654fa34808d382e1050388
Tere Angulo <twilishiba@gmail.com>
2023-10-06 18:44:34 -0600

    added wsgi.py file

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug = True, port=5000)
+    app.run()
